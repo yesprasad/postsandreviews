2e629ec038ac7c87445ed5b65957eeee7f0770f6
yesprasad <y.eshwarprasad@gmail.com>
2019-02-05 09:28:21 +0530

    Updates

---
+++ precache-manifest.56e3a3602aede0ed9733dbf49bbd35e1.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "3d24a288110b5985f0d3",
+    "url": "/postsandreviews/static/js/runtime~main.3d24a288.js"
+  },
+  {
+    "revision": "c3b9dc55a86f5e9644cf",
+    "url": "/postsandreviews/static/js/main.c3b9dc55.chunk.js"
+  },
+  {
+    "revision": "4785ef17b47574d4fea6",
+    "url": "/postsandreviews/static/js/1.4785ef17.chunk.js"
+  },
+  {
+    "revision": "68a3cd257c0a898e9dbc906df9eb1034",
+    "url": "/postsandreviews/index.html"
+  }
+];
